a721979286000d0cafa231e241e586cf0a885499
Lindembergsf2 <166945103+Lindembergsf2@users.noreply.github.com>
2024-05-12 20:35:41 -0300

    images bug fixed

--- vite/meu-app/src/components/Pages/Portfolio/Portfolio.jsx
+++ vite/meu-app/src/components/Pages/Portfolio/Portfolio.jsx
@@ -11,17 +11,17 @@ function Portfolio() {
 const portfolio = [
     {
         link : "https://lindemberglima.netlify.app/",
-        image : "/public/card site.png",
+        image : "./public/card site.png",
         title : "Site pessoal construido em HTML e CSS"
     },
     {
         link : "https://www.instagram.com/p/CrIxZZ8ABTb/?img_index=2",
-        image : "/public/card site 2.png",
+        image : "./public/card site 2.png",
         title : "Material Social Media Loja de Piscinas"
     },
     {
         link : "https://www.instagram.com/p/CrI1VjBgYVg/?img_index=2",
-        image : "/public/card site 3.png",
+        image : "./public/card site 3.png",
         title : "Material Social Media Estetica Automotiva"
     }
 ];
