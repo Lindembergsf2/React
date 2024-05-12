3bab568351114267e7c65c8a343d2cce1adf62ec
Lindembergsf2 <166945103+Lindembergsf2@users.noreply.github.com>
2024-05-12 20:40:58 -0300

    trying to fix images bug

--- vite/meu-app/src/components/Pages/Portfolio/Portfolio.jsx
+++ vite/meu-app/src/components/Pages/Portfolio/Portfolio.jsx
@@ -11,17 +11,17 @@ function Portfolio() {
 const portfolio = [
     {
         link : "https://lindemberglima.netlify.app/",
-        image : "/public/card site.png",
+        image : "https://ibb.co/Htb2yQB",
         title : "Site pessoal construido em HTML e CSS"
     },
     {
         link : "https://www.instagram.com/p/CrIxZZ8ABTb/?img_index=2",
-        image : "/public/card site 2.png",
+        image : "https://ibb.co/8z2PR3q",
         title : "Material Social Media Loja de Piscinas"
     },
     {
         link : "https://www.instagram.com/p/CrI1VjBgYVg/?img_index=2",
-        image : "/public/card site 3.png",
+        image : "https://ibb.co/9wRhyjk",
         title : "Material Social Media Estetica Automotiva"
     }
 ];
